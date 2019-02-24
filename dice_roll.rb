12fc5d68149daba636e321224643e43873ad235f
Matthew Muldrow <jmatthewm92@gmail.com>
2019-02-24 10:54:08 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,8 +1,8 @@
 
-def roll
-  puts roll
-  expect(roll).to be_a(Interger)
-  expect(roll).to be > 0
-  expect(roll).to be < 7
-  expect(roll).to be rand(1..6)
+def #roll
+  puts #roll
+  expect(#roll).to be_a(Interger)
+  expect(#roll).to be > 0
+  expect(#roll).to be < 7
+  expect(#roll).to be rand(1..6)
 end
